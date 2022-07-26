7132ff4b0d74536036b96c07dbf2c19c94ab2cd7
Andrew <Andrew@gmail.com>
2022-07-26 18:11:04 +0300

    Add tasks, gitignore, move hard tests to hardtest dir

---
+++ tasks/medium/pow_2.py
@@ -0,0 +1,24 @@
+"""
+Проверить, является ли число степенью двойки.
+Вернуть True или False
+
+is_pow_2(1) -> True
+is_pow_2(2) -> True
+is_pow_2(16) -> True
+is_pow_2(256) -> True
+is_pow_2(1024) -> True
+is_pow_2(13) -> False
+is_pow_2(17) -> False
+"""
+
+
+def is_pow_2(number) -> bool:
+    return None
+
+
+if __name__ == '__main__':
+    assert is_pow_2(4)
+    assert is_pow_2(16)
+    assert is_pow_2(256)
+    assert not is_pow_2(123)
+    print("Решено")
